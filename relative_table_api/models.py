b5bd76a2603e49adf22bec8a4ec2992a48e2db7f
Conrad Tibesar <conradtibesar@Conrads-MacBook-Pro.local>
2022-02-19 15:50:14 -0600

    create relative table api app

--- relative_table_api/models.py
+++ relative_table_api/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from django.template.defaultfilters import slugify
+from stock_api.models import Stock
 
 
 
 class RelativeTable(models.Model):
     title = models.CharField(max_length=50)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    stocks = models.ManyToManyField(settings.STOCK_MODEL, related_name="relative_tables", blank=True)
+    stocks = models.ManyToManyField(Stock, related_name="relative_tables", blank=True)
     url = models.SlugField(max_length=50, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
